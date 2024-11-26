e02c77df4908ace330cf68134879db5a0d1ec695
mliooh <104557885+mliooh@users.noreply.github.com>
2024-11-26 22:19:08 +0300

    :pencil:

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays message on STDOUT.
+ * @param {String} msg 
+ * @author Alvin Gichuhi <https://github.com/mliooh>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
